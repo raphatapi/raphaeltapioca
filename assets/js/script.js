abf161ef40373e5bb15da931de03427ba0819d37
raphatapi <raphaeltapioca10@gmail.com>
2018-02-24 21:28:31 -0700

    nytreact project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,52 @@
+var open = false;
+
+function Drop(n) {
+	var i;
+	if (open == false) {
+		for (i = n; i < 5; i++) {
+			Drp(i)
+		}
+		open = true
+	} else if (open == true) {
+		for (i = n; i < 5; i++) {
+			Cls(i)
+		}
+		open = false
+	}
+}
+
+function Drp(n) {
+	var elem = document.getElementsByClassName("menu-con")[n];
+	var pos = -1 * window.innerHeight - n * 100;
+	var id = setInterval(frame, 5);
+
+	function frame() {
+		if (pos >= -10) {
+			clearInterval(id);
+			elem.style.top = 0 + 'px';
+		} else {
+			pos += 10;
+			elem.style.top = pos + 'px';
+		}
+	}
+}
+
+function Cls(n) {
+	var elems = document.getElementsByClassName("menu-con")[n];
+	var poss = 0;
+	var ids = setInterval(frames, 5);
+
+	function frames() {
+		if (poss <= -1 * window.innerHeight) {
+			clearInterval(ids);
+			elems.style.top = -1 * window.innerHeight + 'px';
+		} else {
+			poss += -7 - n * 2;
+			elems.style.top = poss + 'px';
+		}
+	}
+}
+
 
 $(document).ready(function(){
     // Add smooth scrolling to all links
@@ -15,7 +64,7 @@ $(document).ready(function(){
         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
         $('html, body').animate({
           scrollTop: $(hash).offset().top
-        }, 800, function(){
+        }, 1200, function(){
      
           // Add hash (#) to URL when done scrolling (default click behavior)
           window.location.hash = hash;
