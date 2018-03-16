068ecf9ce4892a5a283e1bf7d8cc1b02ce1fb585
raphatapi <raphaeltapioca10@gmail.com>
2018-03-16 00:50:56 -0600

    top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,10 +27,7 @@ $(document).ready(function(){
       return false;
     });
 
-    //Add navbar on scroll
-    $(document).on('scroll', function (e) {
-        $('.navbar').css('opacity', ($(document).scrollTop() / 500));
-    });
+    
 
     $('[data-toggle="popover"]', '#upkeep').popover({
       html: true,
@@ -47,3 +44,44 @@ $(document).ready(function(){
     });
 });
 
+$(function() {
+	$(window).scroll(function(){
+		var scrollTop = $(window).scrollTop();
+		if(scrollTop != 0)
+			$('.navbar').stop().animate({'opacity':'0'},0);
+		else	
+			$('.navbar').stop().animate({'opacity':'1'},200);
+	});
+	
+	// $('.navbar').hover(
+	// 	function (e) {
+	// 		var scrollTop = $(window).scrollTop();
+	// 		if(scrollTop != 0){
+	// 			$('.navbar').stop().animate({'opacity':'1'},150);
+	// 		}
+	// 	},
+	// 	function (e) {
+	// 		var scrollTop = $(window).scrollTop();
+	// 		if(scrollTop != 0){
+	// 			$('.navbar').stop().animate({'opacity':'0'},150);
+	// 		}
+	// 	}
+	// );
+});
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if ($('body').scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        $("#myBtn").fadeIn({queue: false, duration: '1s'});
+    } else {
+        $("#myBtn").fadeOut({queue: false, duration: '1s'});
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    $('html, body').animate({scrollTop : 0},600);
+      return false;
+}
